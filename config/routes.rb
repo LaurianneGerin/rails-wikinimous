df1f1b5c4f08aa7a89c7887dffeeb1f57fe18463
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-09 17:59:40 +0200

    Mise en forme des 7 methodes et des views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
-  get 'articles/show'
-
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/update'
-
-  get 'articles/destroy'
-
+root "articles#index"
+resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
